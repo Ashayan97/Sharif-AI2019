db9c448bc170590828c0cf0d533748bf6dcd8c92
Amirhossein Azimyzadeh <39561409+AmirhosseinAzimyzadeh@users.noreply.github.com>
2019-02-12 15:36:01 +0330

    fix Blaster Attack to Sentry --> consider HP of blaster for Killing

--- src/client/Utility_Attack.java
+++ src/client/Utility_Attack.java
@@ -17,6 +17,8 @@ public class Utility_Attack {
     private final static int range_of_bomb = 2;
 
     private final static int damage_of_sentry_ray = 50;
+    private final static int damage_of_blaster_bomb=40;
+    private final static int damage_of_blaster_attack=30;
 
     public static Utility.ATTACK_STATE blasterAttackToHealer(Hero fHero , Hero sHero){
         //first Hero is Blaster and second Hero is Healer
@@ -34,12 +36,22 @@ public class Utility_Attack {
     public static Utility.ATTACK_STATE blasterAttackToSentry(Hero fHero , Hero sHero){
         //first Hero is Blaster and second Hero is Sentry
         //check scape state --> 1- check current HP of our Blaster 2- check state of enemy ray
+        int distance = Utility.Distance(fHero.getCurrentCell(),sHero.getCurrentCell());
+        //check if HP of enemy is less than blaster bomb
+        if(fHero.getAbility(AbilityName.BLASTER_BOMB).isReady()&&
+                distance<=range_of_blaster_bomb+range_of_bomb &&
+                sHero.getCurrentHP()<=damage_of_blaster_bomb){
+            return DORADOR;
+        }
+        //check if we can kill with one attack
+        if(sHero.getCurrentHP()<=damage_of_blaster_attack){
+            return TANBETAN;
+        }
         if(fHero.getCurrentHP()<=damage_of_sentry_ray &&
                 sHero.getAbility(AbilityName.SENTRY_RAY).isReady()) {
             return SCAPE;
         }
         //check if we can hit enemy with bomb -->hdt
-        int distance = Utility.Distance(fHero.getCurrentCell(),sHero.getCurrentCell());
         if(fHero.getAbility(AbilityName.BLASTER_BOMB).isReady() &&
                 distance<=range_of_blaster_bomb+range_of_bomb){
             return DORADOR;
