55c697d57420a3bd203205dbe0ea1f053790b982
Amirhossein Azimyzadeh <39561409+AmirhosseinAzimyzadeh@users.noreply.github.com>
2019-02-12 12:59:27 +0330

    jyghjyhf

--- src/client/Utility.java
+++ src/client/Utility.java
@@ -120,6 +120,7 @@ public class Utility {
     private static ATTACK_STATE CanAttack_Blaster(Hero fHero, Hero sHero) {
         // omidekz
         // ajab
+        //ghjkl
         return ATTACK_STATE.CANTATTACK;
     }
 
