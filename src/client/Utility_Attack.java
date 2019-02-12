56db0f8259cd770bd9f95e2870dda8373db654e5
Amirhossein Azimyzadeh <39561409+AmirhosseinAzimyzadeh@users.noreply.github.com>
2019-02-12 15:13:20 +0330

    complete second method (blaster Attack to Sentry)

--- src/client/Utility_Attack.java
+++ src/client/Utility_Attack.java
@@ -5,14 +5,21 @@ import client.model.Hero;
 import static client.Utility.ATTACK_STATE.DORADOR;
 import static client.Utility.ATTACK_STATE.TANBETAN;
 import static client.Utility.ATTACK_STATE.CANTATTACK;
+import static client.Utility.ATTACK_STATE.SCAPE;
+
+/**
+ * @author :  Amirhossein Azimyzadeh
+ * */
 
 public class Utility_Attack {
     private final static int range_of_blaster_bomb = 5;
     private final static int range_of_blaster_attack = 4 ;
     private final static int range_of_bomb = 2;
+
+    private final static int damage_of_sentry_ray = 50;
+
     public static Utility.ATTACK_STATE blasterAttackToHealer(Hero fHero , Hero sHero){
         //first Hero is Blaster and second Hero is Healer
-        //TODO
         /** if distance is OK and blaster bomb is OK too @return : DORADOR */
         int distance =Utility.Distance(fHero.getCurrentCell(),sHero.getCurrentCell());
         if(fHero.getAbility(AbilityName.BLASTER_BOMB ).isReady() && distance<=range_of_blaster_bomb+range_of_blaster_bomb ){
@@ -26,7 +33,21 @@ public class Utility_Attack {
     }
     public static Utility.ATTACK_STATE blasterAttackToSentry(Hero fHero , Hero sHero){
         //first Hero is Blaster and second Hero is Sentry
-        //TODO
+        //check scape state --> 1- check current HP of our Blaster 2- check state of enemy ray
+        if(fHero.getCurrentHP()<=damage_of_sentry_ray &&
+                sHero.getAbility(AbilityName.SENTRY_RAY).isReady()) {
+            return SCAPE;
+        }
+        //check if we can hit enemy with bomb -->
+        int distance = Utility.Distance(fHero.getCurrentCell(),sHero.getCurrentCell());
+        if(fHero.getAbility(AbilityName.BLASTER_BOMB).isReady() &&
+                distance<=range_of_blaster_bomb+range_of_bomb){
+            return DORADOR;
+        }
+        //check if we can attack with blaster attack
+        if(distance<=range_of_blaster_attack){
+            return TANBETAN;
+        }
         return CANTATTACK;
     }
     public static Utility.ATTACK_STATE blasterAttackToBlaster(Hero fHero , Hero sHero){
