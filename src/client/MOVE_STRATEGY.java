98f8ae10505f46a5fdcb8de7fca03fcc53dc174f
Ashayan97 <shayan_stu@yahoo.com>
2019-02-22 11:23:36 +0330

    Delete 2 useless class

--- src/client/MOVE_STRATEGY.java
+++
@@ -1,9 +0,0 @@
-package client;
-
-import client.model.Cell;
-//locating attack state,attack cell,Des Cell
-public class MOVE_STRATEGY {
-    public ATTACK_STATE attack_state;
-    public Cell AttackCell;
-    public Cell DesCell;
-}
