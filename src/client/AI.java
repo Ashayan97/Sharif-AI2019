4fe3e545cc27378cb2ef63c4a346dbfd99aa857a
Omid Kz <omidekz@gmail.com>
2019-02-24 21:52:59 +0330

    delete world from method params in ai.java

--- src/client/AI.java
+++ src/client/AI.java
@@ -16,21 +16,24 @@ public class AI {
     private Vector<Hero> atttackTo;
     private int whoAttackID;
     private boolean inAttack=false;
-
+    private World world ;
     //****************************************
     void preProcess(World world) {
+        this.world = world;
         objectiveCells = world.getMap().getObjectiveZone();
-        initWallCell(world);
+        initWallCell();
         Blaster.set(world);
     }
 
     void pickTurn(World world) {
-        pickHeroInPhase(world);
+        this.world = world;
+        pickHeroInPhase();
     }
 
     void moveTurn(World world) {
+        this.world = world;
         Utility.printMap(world);
-        init(world);
+        init();
         Blaster.blasterMove(world, world.getMyHeroes()[0],histories[indexOfHeroInHistory(world.getMyHeroes()[0])]);
         Blaster.blasterMove(world, world.getMyHeroes()[1],histories[indexOfHeroInHistory(world.getMyHeroes()[1])]);
         Blaster.blasterMove(world, world.getMyHeroes()[2],histories[indexOfHeroInHistory(world.getMyHeroes()[2])]);
@@ -38,7 +41,8 @@ public class AI {
     }
 
     void actionTurn(World world) {
-        init(world);
+        this.world = world;
+        init();
         Blaster.blasterAttack(world,world.getMyHeroes()[0]);
         Blaster.blasterAttack(world,world.getMyHeroes()[1]);
         Blaster.blasterAttack(world,world.getMyHeroes()[2]);
@@ -50,16 +54,16 @@ public class AI {
     /**
      * this method initialize our need across the phase or turn
      */
-    private void init(World world) {
+    private void init() {
         initHistorys(world.getMyHeroes());
-        initHeroInVision(world);
+        initHeroInVision();
     }
 
-    private void initHeroInVision(World world) {
+    private void initHeroInVision() {
         herosInVision = Utility.getSawHero(world);
     }
 
-    private void initWallCell(World world) {
+    private void initWallCell() {
         if (wallsCell == null) {
             wallsCell = new Vector<>();
             for (Cell[] arryCell : world.getMap().getCells())
@@ -84,7 +88,7 @@ public class AI {
     /**
      * in method mige kodum az hero haye ma(@myHeroes), hero'ye doshman(oppHero) ro didan
      **/
-    private Hero[] whoSeeThisHero(World world, Hero oppHeroe) {
+    private Hero[] whoSeeThisHero(Hero oppHeroe) {
         Vector<Hero> heroes = new Vector<>();
         Hero[] myHeroes = world.getMyHeroes();
         Cell oppHeroCurrentCell = oppHeroe.getCurrentCell();
@@ -97,7 +101,7 @@ public class AI {
     /**
      * we pick our hero for game in this method
      */
-    private void pickHeroInPhase(World world) {
+    private void pickHeroInPhase() {
         switch (PICK_PHASE_COUNTER) {
             case 0:
                 world.pickHero(HeroName.BLASTER);
@@ -130,13 +134,13 @@ public class AI {
         return -1;
     }
 
-    private void move(World world,int HEROID, Cell src,Cell dst, History history,boolean saveCell){
+    private void move(int HEROID, Cell src,Cell dst, History history,boolean saveCell){
         Utility.move(world,HEROID,src,dst);
         if(saveCell)
             history.addLastStep(src);
     }
-    private void move(World world,int HERODID,Cell src,Cell dest,History history){
-        move(world,HERODID,src,dest,history,true);
+    private void move(int HERODID,Cell src,Cell dest,History history){
+        move(HERODID,src,dest,history,true);
     }
 
     private void setGpAttack(Hero whoAttack, Collection<Hero> toAttack){
