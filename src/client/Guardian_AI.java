bf754273a70687cc0c3feec3802da0b61a6cafa9
Amirhossein Azimyzadeh <39561409+AmirhosseinAzimyzadeh@users.noreply.github.com>
2019-02-27 23:14:52 +0330

    fix

--- src/client/Guardian_AI.java
+++ src/client/Guardian_AI.java
@@ -147,7 +147,7 @@ public class Guardian_AI {
         if(guardian.getCurrentHP()==0 || world.getAP()<8)
             return;
         //save AP for Blaster or Sentry
-        if(world.getAP()<30)
+        if(world.getAP()<=30)
             return;
         //if guardian are not in Objective zone :
         if(!isInObjectiveZone()){
